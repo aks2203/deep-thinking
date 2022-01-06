7566719a7c3bd533062b8c8818621ed9c77011a8
Avi Schwarzschild <avi1@umd.edu>
2022-01-06 10:35:38 -0500

    chess warmup period changed.

--- launch/chess_experiments.sh
+++ launch/chess_experiments.sh
@@ -3,6 +3,6 @@ python train_model.py problem.hyp.alpha=0 problem/model=ff_net_recall_2d problem
 python train_model.py problem.hyp.alpha=0 problem/model=dt_net_2d problem=chess name=chess_ablation_no_throttle
 python train_model.py problem.hyp.alpha=0 problem/model=dt_net_recall_2d problem=chess name=chess_ablation_no_throttle
 python train_model.py problem.hyp.alpha=0.5 problem/model=dt_net_2d problem=chess name=chess_ablation_no_throttle
-python train_model.py problem.hyp.alpha=0.5 problem.hyp.warmup_period=10 problem/model=dt_net_recall_2d problem=chess name=chess_ablation_no_throttle
+python train_model.py problem.hyp.alpha=0.5 problem/model=dt_net_recall_2d problem=chess name=chess_ablation_no_throttle
 python train_model.py problem.hyp.alpha=1 problem/model=dt_net_2d problem=chess name=chess_ablation_no_throttle
 python train_model.py problem.hyp.alpha=1 problem/model=dt_net_recall_2d problem=chess name=chess_ablation_no_throttle
